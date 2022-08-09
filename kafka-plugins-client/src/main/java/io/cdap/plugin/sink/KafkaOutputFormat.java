47fac0a6eaadd2f1995fb434b8b980e5203fa82a
katukota <katukota@google.com>
2022-08-09 15:17:19 -0700

    Kafka Sink Performance Improvement 1. Make producer.send() function asynchronous 2. Set property linger.ms to a default value of 100ms which will make the producer wait for 100ms so that messages can be sent out as a batch.

--- kafka-plugins-client/src/main/java/io/cdap/plugin/sink/KafkaOutputFormat.java
+++ kafka-plugins-client/src/main/java/io/cdap/plugin/sink/KafkaOutputFormat.java
@@ -39,6 +39,8 @@ import java.util.Properties;
  */
 public class KafkaOutputFormat extends OutputFormat<Text, Text> {
   private static final Logger LOG = LoggerFactory.getLogger(KafkaOutputFormat.class);
+  // default value found with experiments
+  private static final int DEFAULT_LINGER_MS_CONFIG = 100;
 
   private KafkaProducer<String, String> producer;
 
@@ -85,46 +87,52 @@ public class KafkaOutputFormat extends OutputFormat<Text, Text> {
 
     // CDAP-9178: cached the producer object to avoid being created on every batch interval
     if (producer == null) {
-      Properties props = new Properties();
-      // Configure the properties for kafka.
-      props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-              configuration.get(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG));
-      props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-              configuration.get(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG));
-      props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-              configuration.get(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG));
-      props.put("compression.type", configuration.get("compression.type"));
-      props.put(ProducerConfig.LINGER_MS_CONFIG, 100);
-
-      if (!Strings.isNullOrEmpty(configuration.get("hasKey"))) {
-        // set partitioner class only if key is provided
-        props.put("partitioner.class", "io.cdap.plugin.sink.StringPartitioner");
-      }
-
-      if (configuration.get("async") != null && configuration.get("async").equalsIgnoreCase("true")) {
-        props.put("producer.type", "async");
-        props.put(ProducerConfig.ACKS_CONFIG, "1");
-      } else {
-        props.put("producer.type", "sync");
-      }
-
-      Map<String, String> valByRegex = configuration.getValByRegex("^additional.");
-
-      for (Map.Entry<String, String> entry : valByRegex.entrySet()) {
-        //strip off the prefix we added while creating the conf.
-        props.put(entry.getKey().substring(11), entry.getValue());
-        LOG.info("Property key: {}, value: {}", entry.getKey().substring(11), entry.getValue());
-      }
-
-      // Add Kerberos login information if any
-      if (!Strings.isNullOrEmpty(configuration.get(KafkaHelpers.SASL_JAAS_CONFIG))) {
-        props.put(KafkaHelpers.SASL_JAAS_CONFIG, configuration.get(KafkaHelpers.SASL_JAAS_CONFIG));
-      }
-
-      producer = new org.apache.kafka.clients.producer.KafkaProducer<>(props);
+      producer = initializeProducer(configuration);
     }
 
     return new KafkaRecordWriter(producer, topic);
+  }
+
+  private KafkaProducer<String, String> initializeProducer(Configuration configuration) {
+    Properties props = new Properties();
+    // Configure the properties for kafka.
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+            configuration.get(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG));
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+            configuration.get(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG));
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+            configuration.get(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG));
+    props.put("compression.type", configuration.get("compression.type"));
+
+
+    if (!Strings.isNullOrEmpty(configuration.get("hasKey"))) {
+      // set partitioner class only if key is provided
+      props.put("partitioner.class", "io.cdap.plugin.sink.StringPartitioner");
+    }
+
+    if (configuration.get("async") != null && configuration.get("async").equalsIgnoreCase("true")) {
+      props.put("producer.type", "async");
+      props.put(ProducerConfig.ACKS_CONFIG, "1");
+    } else {
+      props.put("producer.type", "sync");
+    }
+
+    Map<String, String> valByRegex = configuration.getValByRegex("^additional.");
+
+    for (Map.Entry<String, String> entry : valByRegex.entrySet()) {
+      //strip off the prefix we added while creating the conf.
+      props.put(entry.getKey().substring(11), entry.getValue());
+      LOG.info("Property key: {}, value: {}", entry.getKey().substring(11), entry.getValue());
+    }
+
+    // Add Kerberos login information if any
+    if (!Strings.isNullOrEmpty(configuration.get(KafkaHelpers.SASL_JAAS_CONFIG))) {
+      props.put(KafkaHelpers.SASL_JAAS_CONFIG, configuration.get(KafkaHelpers.SASL_JAAS_CONFIG));
+    }
+
+    // Set linger.ms property to a default value if not specified by user.
+    props.putIfAbsent(ProducerConfig.LINGER_MS_CONFIG, DEFAULT_LINGER_MS_CONFIG);
+    return new KafkaProducer<>(props);
   }
 }
 
